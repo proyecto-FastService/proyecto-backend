557a5ab353ceffebc0fe96809aeec05d26e2496b
adrianpeinado2003 <adrianpeinado2003@gmail.com>
2023-05-04 19:51:16 +0200

    modelos

---
+++ app/Models/Producto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+use App\Models\Mesa;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $table = "productos";
+
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        'nombre',
+        'existencias',
+        'alergenos',
+        'precios',
+        'descripcion',
+        'ingredientes',
+    ];
+
+    public function mesa(){
+        return $this->hasMany(Mesa::class);
+    }
+
+    public function mesas(){
+        return $this->belongsTo(Mesa::class);
+    }
+}
