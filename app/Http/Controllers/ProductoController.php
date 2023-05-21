7923c88578ed2eac5bae69a50c5b7c88b50e7e38
Rafa <castroserranorafael@gmail.com>
2023-05-21 13:16:03 +0200

    cambiados token

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,9 +6,26 @@ use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
-    //
 
     public function obtenerTodosProductosConStock(){
         return Producto::obtenerTodosProductosEnStock();
     }
+
+    public function admAddProducto(){
+        // el administrador añade un producto entero por lo que nos deben pasar todos los detalles del producto, crear el objeto y meterlo en la base de datos
+    }
+
+    public function admOcultarProducto($idProducto){
+        // el administrador pone el stock a 0 de un producto para que no se muestre a los cliente
+    }
+
+    public function admEditarProducto($producto){
+        // recogemos el objeto producto y hacemos un flush con la id
+    }
+
+    public function admObtenerListadoProducto(){
+        // recogemos todos los productos con y sin stock y se los devolvemos a la vista para que los muestre y los administre
+    }
+
+
 }
